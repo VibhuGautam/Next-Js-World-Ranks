8e82dd513646fe5db215136954ecefcc98ddd58c
VibhuGautam <vibhuparmar13@gmail.com>
2021-05-17 09:07:00 +0530

    Filtered Changes in the SearchInput

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,51 @@
-import { useState } from 'react'
-import CountriesTable from '../components/CountriesTable/CountriesTable'
-import Layout from '../components/Layout/Layout'
-import SearchInput from '../components/SearchInput/SearchInput'
+import Head from "next/head";
+import { useState } from "react";
+import CountriesTable from "../components/CountriesTable/CountriesTable";
+import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearchInput";
+import styles from "../styles/Home.module.css";
 
-import styles from '../styles/Home.module.css'
-export default function Home({countries}) {
-    // console.log(countries)
-    const [keyword, setKeyword] = useState('')
+export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
 
-    const filteredCountries = countries.filter(country => 
-        country.name.toLowerCase().includes(keyword)) || 
-        country.region.toLowerCase().includes(keyword) ||
-        country.subregion.toLowerCase().includes(keyword)
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
 
-    const onInputChange = (e)  => {
-        e.preventDefault();
+  const onInputChange = (e) => {
+    e.preventDefault();
 
-        setKeyword(e.target.value.toLowerCase());
-    }
+    setKeyword(e.target.value.toLowerCase());
+  };
 
-    return( 
-        <Layout>
-            <div className = {styles.inputContainer}>
-                <div className = {styles.counts}>
-                    Found {countries.length} countries 
-                </div>
-                <div className = {styles.input}>
-                    <SearchInput placeholder = "Filter by Name, Region or SubRegion" onChange = {onInputChange} />
-                </div>
-            </div>
-            <CountriesTable countries = {filteredCountries} />
-        </Layout>
-        
-    )
+  return (
+    <Layout>
+      <div className={styles.inputContainer}>
+        <div className={styles.counts}>Found {countries.length} countries</div>
+
+        <div className={styles.input}>
+          <SearchInput
+            placeholder="Filter by Name, Region or SubRegion"
+            onChange={onInputChange}
+          />
+        </div>
+      </div>
+
+      <CountriesTable countries={filteredCountries} />
+    </Layout>
+  );
 }
 
-export const getStaticProps = async() => {
-    const res = await fetch('https://restcountries.eu/rest/v2/all');
-    const countries = await res.json();
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
 
-    return {
-        props:{
-            countries,
-        }
-    }
-} 
+  return {
+    props: {
+      countries,
+    },
+  };
+};
